46403da1407dc1b874b1dedaf3867faccbac4ec2
benlow22 <ben2.2low@gmail.com>
2022-09-23 18:22:15 -0700

    Add postgres connection string to app and notes

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')      // import express module
 const app = express()                   // create instance of express application = can use methods (get, listen, put, ...) from express
+const { Client } = require('pg');
+var connectionString = "postgres://postgres:postgres@localhost:5432/E-Commercer_marketplace-project";
+const client = new Client({
+    connectionString: connectionString
+    });
 const port = 3000                       // store port number = endpoint communication
+
+
 
 app.get('/', (req, res) => {            // create get method at endpoint localhost:port3000/
     res.send('Hello World!')            // respond with 'Hello World!'
