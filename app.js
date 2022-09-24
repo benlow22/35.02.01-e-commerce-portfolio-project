9e5710f67e743a838da566fb7bc61f72f1679741
benlow22 <ben2.2low@gmail.com>
2022-09-23 18:34:53 -0700

    add db connection code to postgres to app.js

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')      // import express module
-const app = express()                   // create instance of express application = can use methods (get, listen, put, ...) from express
 const { Client } = require('pg');
 var connectionString = "postgres://postgres:postgres@localhost:5432/E-Commercer_marketplace-project";
 const client = new Client({
     connectionString: connectionString
+});
+
+client.connect();
+
+const app = express()                   // create instance of express application = can use methods (get, listen, put, ...) from express
+const port = 5432                       // store port number = endpoint communication
+
+app.set('port', process.env.PORT || 4000);
+
+app.get('/users', function (req, res, next) {
+    client.query('SELECT * FROM users', [1], function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
     });
-const port = 3000                       // store port number = endpoint communication
-
-
+});
 
 app.get('/', (req, res) => {            // create get method at endpoint localhost:port3000/
     res.send('Hello World!')            // respond with 'Hello World!'
